696b3ebd00d45f23f879172393ebb0f8258eb5e2
tasyanozukaa <tasyanozuka@student.telkomuniversity.ac.id>
2021-05-01 23:59:12 +0700

    Memperbaiki fitur Mengikuti Kegiatan ref #7

--- resources/views/mengikutikegiatan.blade.php
+++ resources/views/mengikutikegiatan.blade.php
@@ -16,7 +16,7 @@
     <table class="table table-striped table-hover">
         <thead class="thead-dark">
             <tr>
-                <th scope="col">No</th>
+                {{-- <th scope="col">No</th> --}}
                 <th scope="col">Nama Kegiatan</th>
                 <th scope="col">Jenis Kegiatan</th>
                 <th scope="col">Tanggal Kegiatan</th>
@@ -24,37 +24,66 @@
                 <th scope="col">Action</th>
             </tr>
         </thead>
-        {{-- <tbody>
-            @foreach 
-            @if ($hadirPelatihan as $hp)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $hp->nama_pelatihan }}</td>
-                    <td>{{ $hp->jenis_kegiatan }}</td>
-                    <td>{{ $hp->tgl_pelatihan }}</td>
-                    <td>{{ $hp->jam_pelatihan }}</td>
-                    <td>
-                            <a href="{{ $hp->link }}" class="btn btn-block btn-success">Hadir</a>
-                        @endif
-                    </td>
-                </tr>
-            @endif
-            @if ($hadirRapat as $hr)
+        <tbody>
+            @foreach ($rapat as $hr)    
             <tr>
-                <td>{{ $loop->iteration }}</td>
+                @if($hr->status_aproval !== "waiting")
+                {{-- <td>{{ $loop->iteration }}</td> --}}
                 <td>{{ $hr->nama_rapat }}</td>
                 <td>{{ $hr->jenis_kegiatan }}</td>
                 <td>{{ $hr->tgl_rapat }}</td>
                 <td>{{ $hr->jam_rapat }}</td>
                 <td>
-                        <a href="{{ $hp->link }}" class="btn btn-block btn-success">Hadir</a>
-                    @endif
+                    @php
+                        $link = "http://$hr->link"
+                    @endphp
+                    {{-- <a id="mylink" class="btn btn-sm btn-success">Hadir</a> --}}
+                    <form action="{{route('absen')}}" method="POST">
+                        @csrf
+                        <a target="_blank" href="{{$link}}" class="btn btn-sm btn-success">Link Disini</a>
+                        <button class="btn btn-sm btn-success" type="submit">Absen</button>
+                    </form>
                 </td>
             </tr>
-        @endif
+            @endif
             @endforeach
-        </tbody> --}}
+            @foreach ($pelatihan as $pl)  
+            @if($pl->status_aproval !== "waiting")  
+            <tr>
+                {{-- <td>{{ $loop->iteration }}</td> --}} 
+                <td>{{ $pl->nama_pelatihan }}</td>
+                <td>{{ $pl->jenis_kegiatan }}</td>
+                <td>{{ $pl->tgl_pelatihan }}</td>
+                <td>{{ $pl->jam_pelatihan }}</td>
+                <td>
+                    @php
+                        $links = "http://$pl->link"
+                    @endphp
+                    {{-- <a id="pelatihan" class="btn btn-sm btn-success">Hadir</a> --}}
+                    <form action="{{route('absen')}}" method="POST">
+                        @csrf
+                        <a target="_blank" href="{{$links}}" class="btn btn-sm btn-success">Link Disini</a>
+                        <button class="btn btn-sm btn-success" type="submit">Absen</button>
+                    </form>
+                </td>
+            </tr>
+            @endif
+            @endforeach
+        </tbody>
     </table>
 </div>
+
+{{-- js  buat misal link di klik dia buka 2 site, 1 buat jadiin kalo dia hadir --}}
+{{-- <script> 
+mylink.onclick = function(){
+    open('{{$link}}');
+    location.href = ('https://www.gmail.com');
+}
+
+pelatihan.onclick = function(){
+    open('{{$links}}');
+    location.href = ('https://www.gmail.com');
+}
+</script> --}}
 <!-- end of konten -->
 @endsection
