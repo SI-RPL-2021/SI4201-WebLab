359e8a1a21fe982c47e25bd44b0685ca498fa164
Faizal Hudya R <rizfianto@gmail.com>
2021-06-05 09:30:08 +0700

    adding models to Dokumentasi ref #8

---
+++ app/Models/Dokumentasi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dokumentasi extends Model
+{
+    use HasFactory;
+    protected $table = 'dokumentasi';
+    protected $fillable = ['id_dokumentasi','foto'];
+}
