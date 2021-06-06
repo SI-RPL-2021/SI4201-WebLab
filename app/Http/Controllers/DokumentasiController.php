5cfdd18beb843d0bb9eca903206a949ae3c29309
Faizal Hudya R <rizfianto@gmail.com>
2021-06-06 16:30:51 +0700

    Membenarkan routing dokumentasiController dan mengimplementasi cara baru untuk memasukan foto di database ref #8

--- app/Http/Controllers/DokumentasiController.php
+++ app/Http/Controllers/DokumentasiController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Rapat;
+use App\Models\Pelatihan;
+use App\Models\Anggota;
+use App\Models\Absenrapat;
 use Illuminate\Http\Request;
-use App\Models\Dokumentasi;
+use App\Models\DokumentasiRapat;
+use App\Models\DokumentasiPelatihan;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DokumentasiController extends Controller
 {
@@ -14,29 +21,64 @@ class DokumentasiController extends Controller
         return view('upload_dokumentasi');
     }
 
-    public function store(Request $request)
+    public function uploadDokumentasiRapat(Request $request){
+        $rapat = Rapat::all();
+        return view ('sekretaris.upload_dokumentasiRapat', ['rapat' => $rapat]);
+    }
+
+    public function uploadDokumentasiPelatihan(Request $request){
+        $pelatihan = Pelatihan::all();
+        return view ('sekretaris.upload_dokumentasiPelatihan', ['Pelatihan' => $pelatihan]);
+    }
+
+    public function storeRapat(Request $request)
     {
-        $dokumentasi = new Dokumentasi();
 
-        $dokumentasi->name = $request->input('foto');
+        $size = $request->file('foto')->getSize();
+        $name = $request->file('foto')->getClientOriginalName();
 
-        if ($request->hasfile('foto'))
-        {
-            $file = $request->file('foto');
-            $extension = $file->getClientOriginalExtension(); //mendapatkan image extension
-            $filename = time() . '.' . $extension;
-            $file->move('uploads/dokumentasi/', $filename);
-            $dokumentasi->foto = $filename;
-        }   else {
-            return $request;
-            $dokumentasi->foto = '';
-        }
+        $request->file('foto')->storeAs('public/images/', $name);
+        $foto = new DokumentasiRapat();
+        $foto->name = $name;
+        $foto->size = $size;
+        $foto->save();
+        return redirect()->back();
 
-        $dokumentasi->save();
+        // $dokumentasi = new Dokumentasi();
 
-        return view('dokumentasi')->with('dokumentasi',$dokumentasi);
+        // $dokumentasi->name = $request->input('foto');
+
+        // if ($request->hasfile('foto'))
+        // {
+        //     $file = $request->file('foto');
+        //     $extension = $file->getClientOriginalExtension(); //mendapatkan image extension
+        //     $filename = time() . '.' . $extension;
+        //     $file->move('uploads/dokumentasi/', $filename);
+        //     $dokumentasi->foto = $filename;
+        // }   else {
+        //     return $request;
+        //     $dokumentasi->foto = '';
+        // }
+
+        // $dokumentasi->save();
+
+        // return view('dokumentasi')->with('dokumentasi',$dokumentasi);
 
 
+    }
+
+    public function storePelatihan(Request $request)
+    {
+
+        $size = $request->file('foto')->getSize();
+        $name = $request->file('foto')->getClientOriginalName();
+
+        $request->file('foto')->storeAs('public/images/', $name);
+        $foto = new DokumentasiPelatihan();
+        $foto->name = $name;
+        $foto->size = $size;
+        $foto->save();
+        return redirect()->back();
     }
 
 }
